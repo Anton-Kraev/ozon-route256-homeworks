9e7f4d29af63b089df0eac88881aabe6ac8a1525
Anton Kraev <antonkraeww@gmail.com>
2024-06-01 00:16:32 +0300

    GOHW-1: cli logic divided into several files

---
+++ hw-1/internal/cli/refunds_list.go
@@ -0,0 +1,31 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+)
+
+func (c CLI) refundsList(args []string) error {
+	var pageN, perPage uint
+
+	fs := flag.NewFlagSet(refundsList, flag.ContinueOnError)
+	fs.UintVar(&pageN, "pageN", 0, "use --pageN=3")
+	fs.UintVar(&perPage, "perPage", 0, "use --perPage=10")
+
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	refunds, err := c.Module.RefundsList(pageN, perPage)
+	if err == nil {
+		fmt.Println("\nRefunds list:")
+		for _, refund := range refunds {
+			fmt.Printf(
+				"  orderID=%d clientID=%d refunded=%s\n",
+				refund.OrderID, refund.ClientID, refund.StatusChanged,
+			)
+		}
+	}
+
+	return err
+}
