cb3801d25aa3864c7e321f8a9755eeeb4bfecce5
Anton Kraev <antonkraeww@gmail.com>
2024-06-14 22:32:04 +0300

    GOHW-3: add docker support

--- hw-3/cmd/main.go
+++ hw-3/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import "gitlab.ozon.dev/antonkraeww/homeworks/hw-3/internal/app"
 
-const configPath = "./configs/workers.json"
+const configPath = "../configs/workers.json"
 
 func main() {
 	app.Start(parseArgs(), configPath)
